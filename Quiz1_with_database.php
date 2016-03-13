79097ba6277792a3883b4ec7561ba01ac7fcc69c
Suny Kim <ynux@gmx.net>
2016-03-13 15:34:05 +0100

    works, offering answers, for question 1 only and without chck

--- Quiz1_with_database.php
+++ Quiz1_with_database.php
@@ -10,22 +10,19 @@
   $db = mysqli_connect("localhost","root","");
   mysqli_set_charset($db, "latin1");
   mysqli_select_db($db, "quiz");
+  echo "<form action=\"html/quiz/Quiz1_with_database.php\" method=\"post\">
+  <select name=antwort> ";
   $fragen_id = 1;
   $sql = "SELECT * FROM antworten WHERE $fragen_id = 1;";
   $result = mysqli_query($db, $sql);
   while ($row = mysqli_fetch_assoc($result)) {
     $antwortArray = [];
-    $antwort[$row['antwort_id']] = $row['antwort_text'];
-    echo "<p> Antwort $row[antwort_id] ist $row[antwort_text] </p>";
-    
-    #  echo "<form action=\"html/quiz/Quiz1_with_database.php\" method=\"post\">
-    #  <select name=$antwort>
-    #  <option>$Antwort1</option>
-    #  <option>$Antwort2</option>
-    #  <option>$Antwort3</option>
-    #  </select>
-    #  <input type=\"submit\" value=\"&Uuml;berpr&uuml;fen\"></form><br>"; 
+    $antwortArray[$row['antwort_id']] = $row['antwort_text'];
+    echo "<option>$row[antwort_text] </option>";
   }
+    echo " </select>
+    <input type=\"submit\" value=\"&Uuml;berpr&uuml;fen\"></form><br>"; 
+  
     ?>
   </body>
 </html>
