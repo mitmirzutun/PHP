c4903547812eb7e45d54b71c1be9c2ee75d49be8
mitmirzutun <milan1111@gmx.de>
2018-09-07 10:06:04 +0200

    Bugfix, Performance upgrade, all endings accepted

--- clear.php
+++ clear.php
@@ -1,6 +1,6 @@
 <?php
 //Defining the files
-$addNA = False;
+$valueWhenTheresNoData = "";
 $in = file("migr_asyappctza.tsv");
 $out = fopen("migr_asyappctza_unpivot.csv", "w+");
 
@@ -8,20 +8,20 @@ $out = fopen("migr_asyappctza_unpivot.csv", "w+");
 fwrite($out, "citizen,sex,unit,age,asyl_app,geo,time,ammount\n");
 
 $in_array_line1 = explode("\t",$in[0]);
+//The year comes in in a faulty format, so correct it to the right one
+for ($i = 1; $i < count($in_array_line1); $i++) {
+  $year = "";
+  for ($j = 0; $j < 4; $j++) {
+    $year .= $in_array_line1[$i][$j];
+  }
+  $in_array_line1[$i] = $year;
+}
 for ($k = 1; $k < count($in); $k++) {
-  $in_array_lineK = explode("\t",$in[$k]);
+  $in_array_lineK = explode("\t",trim($in[$k],"\n"));
   for($i = 1; $i < count($in_array_line1); $i++){
-    $jear = "";   //Correcting the year
-    for ($j = 0; $j < 4; $j++) {
-      $year .= $in_array_line1[$i][$j];
-    }
     $data_out = $in_array_lineK[0] . "," . $year . ",";
     if ($in_array_lineK[$i] === ": " or $in_array_lineK[$i] === ": \n") {
-      if ($addNA == True) {
-        $data_out .= "N/A";
-      } elif ($addNA == False) {
-        $data_out .= "";
-      }
+      $data_out .= $valueWhenTheresNoData;
     } else {
       $data_out .= $in_array_lineK[$i];
     }
